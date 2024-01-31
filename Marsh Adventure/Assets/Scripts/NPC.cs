eef08b33564a842d4acca44bc67adbf9863ee62a
Zironix00 <zemiguel.santos2000@gmail.com>
2024-01-31 11:50:58 +0000

    assets

---
+++ Marsh Adventure/Assets/Scripts/NPC.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class NPC : MonoBehaviour
+{
+   public GameObject dialoguePanel;
+   public TMP_Text  dialogueText;
+   public string[] dialogue;
+   private int index;
+
+   public GameObject contButton;
+   public float wordSpeed;
+   public bool playerIsClose;
+   public bool isEnd;
+
+   public bool isKey;
+
+   public GameObject door;
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.E) && playerIsClose)
+       {
+        if(dialoguePanel.activeInHierarchy)
+        {
+            zeroText();
+        }
+        else
+        {
+            dialoguePanel.SetActive(true);
+            StartCoroutine(Typing());
+        }
+       }
+
+       if(dialogueText.text == dialogue[index])
+       {
+        contButton.SetActive(true);
+       }
+    }
+
+    public void zeroText()
+    {
+        dialogueText.text = "";
+        index= 0;
+        dialoguePanel.SetActive(false);
+    }
+
+    IEnumerator Typing()
+    {
+        foreach (char letter in dialogue[index]. ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+
+        contButton.SetActive(false);
+
+        if(index < dialogue.Length -1)
+        {
+          index++;  
+          dialogueText.text = "";
+          StartCoroutine(Typing());
+        }
+        else
+        {
+            zeroText();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {   
+        if(isEnd == true)
+       {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+       }
+        if(other.CompareTag("Player")) 
+       {
+        playerIsClose = true;
+       }
+        if(isKey == true)
+       {
+        Debug.Log("key picked up");
+        door.SetActive(false);
+       }
+    }
+       
+       private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player")) 
+       {
+        playerIsClose = false;
+        zeroText();
+       }
+    }
+}
